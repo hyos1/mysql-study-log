fa3622288fab7e22bd3c5d81ae9af4ba702243ce
hyos <hyozzang99@gmail.com>
2025-08-12 20:11:14 +0900

    Feat: 값이 없을 때 대체 값과 문자열 분리

--- 문제풀이3.sql
+++ 문제풀이3.sql
@@ -19,3 +19,23 @@ SELECT
 	name,
 	COALESCE(description, name) AS product_display_info
 FROM products;
+
+-- 문제4: products 테이블에 description (상품 설명) 컬럼과 name (상품명) 컬럼이 있다. 상품 정보를 표시할 때, 
+-- description 값이 존재하면 그 값을 사용하고, description 이 NULL 이면 name 값을 사용하고, 만약 name
+-- 값도 NULL 이라면(그런 경우는 없지만 가정) '정보 없음'이라는 문구를 출력하고 싶다. 이 로직을 COALESCE() 함수
+-- 를 사용하여 display_text 라는 별칭으로 출력하라.
+SELECT
+	name,
+    description,
+    COALESCE(description, name, '정보 없음') AS display_text
+FROM products;
+
+-- 문제5: customers 테이블의 email 컬럼에서 아이디 부분만 추출하고, 아이디의 글자 수를 계산하라. 아이디는 @ 앞부분
+-- 의 문자열이다. SUBSTRING_INDEX() 와 CHAR_LENGTH() 함수를 활용하여 user_id 와 id_length 라는 별칭
+-- 으로 결과를 출력하라
+SELECT
+	email,
+	SUBSTRING_INDEX(email, '@', 1) AS user_id,
+	CHAR_LENGTH(SUBSTRING_INDEX(email, '@', 1)) AS id_length
+FROM
+ customers;
