f2cdff393cef95c1626690839b8cb1e26d988dcf
hyos <hyozzang99@gmail.com>
2025-10-18 21:53:21 +0900

    Test: 고객별 총 구매액 계산

--- SQL - 기본/내부 조인/문제풀이.sql
+++ SQL - 기본/내부 조인/문제풀이.sql
@@ -21,3 +21,16 @@ FROM orders o
 JOIN customers c ON o.customer_id = c.customer_id
 JOIN products p ON o.product_id = p.product_id
 WHERE o.status = '주문접수';
+
+-- 문제 3번
+-- INNER JOIN 과 집계 함수를 함께 사용해서, 각 고객이 지금까지 주문한 총 구매액을 계산해라. 결과는 고객 이름
+-- (user_name) 과 총 구매액(total_purchase_amount) 으로 구성되어야 하며, 총 구매액이 높은 순서대로 정렬
+-- 해야 한다. ( 총 구매액 = 주문수량 * 상품가격 )
+SELECT
+	u.name AS user_name,
+    SUM(o.quantity * p.price) AS total_purchase_amount
+FROM orders o
+JOIN users u ON o.user_id = u.user_id
+JOIN products p ON o.product_id = p.product_id
+GROUP BY u.name
+ORDER BY total_purchase_amount DESC;
